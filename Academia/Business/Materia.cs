a4892f1a991b0807cb0f6607a9732eeeab430de6
brunobranca <bruno.branca91@gmail.com>
2015-04-21 14:20:47 -0300

    agregada materia

---
+++ Academia/Business/Materia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    class Materia : BusinessEntity
+    {
+        String Descripcion { get; set; }
+        int HSSemanales { get; set; }
+        int HSTotales { get; set; }
+        int IDPlan { get; set; }
+    }
+}
